d5b8b8f76c4f1c163c4a3671b30ffd5a859fa917
Greg Izatt <gizatt@mit.edu>
2017-12-21 14:26:55 -0500

    Add data downloading utilities for google images And a crop-many-images script

---
+++ dataset_generation/README.md
@@ -0,0 +1,1 @@
+Google images download script based on work from https://github.com/hardikvasa/google-images-download
---
+++ dataset_generation/randomly_crop_images_in_folder.py
@@ -0,0 +1,30 @@
+import random, os, time
+from PIL import Image
+
+in_folder = "data_in"
+out_folder = "data_out"
+os.system("mkdir -p " + out_folder)
+
+dx = dy = 500
+tilesPerImage = 10
+
+folders = os.listdir(in_folder)
+k = 0
+for folder in folders:
+    files = os.listdir(in_folder + "/" + folder + "/")
+    for file in files:
+       try:
+           with Image.open(os.path.join(in_folder, folder, file)) as im:
+             for i in range(1, tilesPerImage+1):
+               w, h = im.size
+               if w > dx and h > dy:
+                   x = random.randint(0, w-dx-1)
+                   y = random.randint(0, h-dy-1)
+                   print("Cropping {}: {},{} -> {},{}".format(file, x,y, x+dx, y+dy))
+                   newname = "%05d.jpg" % k
+                   im.crop((x,y, x+dx, y+dy))\
+                     .save(os.path.join(out_folder, newname))
+                   k += 1
+       except Exception as e:
+           print "Exception caught: ", e
+
